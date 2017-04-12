dd7c839055a94e67a2d8ffda31c75c32c9c55ec6
doppelgunner <doppelgunner@gmail.com>
2017-04-12 21:42:59 +0800

    Snooper_1

---
+++ Snooper.java
@@ -0,0 +1,45 @@
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
+
+import java.util.logging.*;
+
+public class Snooper implements NativeKeyListener {
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+
+        if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+            try {
+				GlobalScreen.unregisterNativeHook();
+			} catch (Exception ex) {
+				
+			}
+        }
+    }
+
+    public void nativeKeyReleased(NativeKeyEvent e) {
+        System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+    }
+
+    public void nativeKeyTyped(NativeKeyEvent e) {
+        System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
+    }
+
+    public static void main(String[] args) {
+		Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+		logger.setLevel(Level.OFF);
+		logger.setUseParentHandlers(false);
+        try {
+            GlobalScreen.registerNativeHook();
+        }
+        catch (NativeHookException ex) {
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+
+            System.exit(1);
+        }
+
+        GlobalScreen.addNativeKeyListener(new Snooper());
+    }
+}
